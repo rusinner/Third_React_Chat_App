a1ec5ae3dbb1c8729185ede82f5c939fe8fba5a5
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-30 14:10:34 +0200

    TeamChannelPreview

--- client/chat-app/src/components/ChannelListContainer.jsx
+++ client/chat-app/src/components/ChannelListContainer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import {
+  ChannelSearch,
+  TeamChannelList,
+  TeamChannelPreview,
+} from "../components";
 import Cookies from "universal-cookie";
 import HospitalIcon from "../assets/hospital.png";
 import LogoutIcon from "../assets/logout.png";
@@ -33,6 +37,25 @@ const ChannelListContainer = () => {
       <SideBar />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
+          )}
+        />
       </div>
     </>
   );
--- client/chat-app/src/components/index.js
+++ client/chat-app/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as ChannelContainer } from "../components/ChannelContainer";
 export { default as ChannelListContainer } from "../components/ChannelListContainer";
 export { default as ChannelSearch } from "../components/ChannelSearch";
+export { default as TeamChannelList } from "../components/TeamChannelList";
+export { default as TeamChannelPreview } from "../components/TeamChannelPreview";
