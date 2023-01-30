35a8270d3718ba434942f4276ded02cd720befd3
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-30 13:03:26 +0200

    ChannelSearch

--- client/chat-app/src/components/ChannelListContainer.jsx
+++ client/chat-app/src/components/ChannelListContainer.jsx
@@ -3,7 +3,9 @@ import { ChannelList, useChatContext } from "stream-chat-react";
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 import Cookies from "universal-cookie";
 import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
 
+//sidebar component
 const SideBar = () => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
@@ -11,11 +13,29 @@ const SideBar = () => (
         <img src={HospitalIcon} alt="hospital" width="30" />
       </div>
     </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Group Pager</p>
   </div>
 );
 
 const ChannelListContainer = () => {
-  return <div>ChannelListContainer</div>;
+  return (
+    <>
+      <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
+    </>
+  );
 };
 
 export default ChannelListContainer;
