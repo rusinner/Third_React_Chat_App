f674df6ab0cdf7f4be4b045632741d3673551d76
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-31 13:21:06 +0200

    ChannelInner

--- client/chat-app/src/App.jsx
+++ client/chat-app/src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./App.css";
+import "stream-chat-react/dist/css/index.css";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
@@ -27,12 +29,27 @@ if (authToken) {
 }
 
 function App() {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
