ee70e9f93cccfdace95a161be057aed1216d28cb
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-02 13:42:57 +0200

    Edit Channel

--- client/chat-app/src/components/EditChannel.jsx
+++ client/chat-app/src/components/EditChannel.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
 
-const EditChannel = () => {
-  return <div>EditChannel</div>;
+import { UserList } from "./UserList";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+    setChannelName(event.target.value);
+  };
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add Members</p>
+    </div>
+  );
+};
+
+const EditChannel = ({ setIsEditing }) => {
+  const { channel } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name);
+  return (
+    <div className="edit-channel__container">
+      <div className="edit-channel__header">
+        <p>Edit Channel</p>
+        <CloseCreateChannel setIsEditing={setIsEditing} />
+      </div>
+      <ChannelNameInput
+        channelName={channelName}
+        setChannelName={setChannelName}
+      />
+    </div>
+  );
 };
 
 export default EditChannel;
